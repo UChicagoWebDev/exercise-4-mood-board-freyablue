a8debdabd899536bce835b8b11b82279a98a7bdf
yijia <yijiac@uchicago.edu>
2024-02-03 16:16:56 -0600

    scd

--- mood.js
+++ mood.js
@@ -1,6 +1,8 @@
 //mood.js
 const bing_api_endpoint = "https://api.bing.microsoft.com/v7.0/images/search";
-const bing_api_key = BING_API_KEY
+BING_API_KEY = "a506e7c8978b49f88d844976daa5d6b0";
+const bing_api_key = BING_API_KEY;
+
 
 function runSearch() {
 
@@ -17,11 +19,12 @@ function runSearch() {
   // TODO: Construct the request object and add appropriate event listeners to
   // handle responses. See:
   // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest_API/Using_XMLHttpRequest
-  request.open();
-  request.setRequestHeader("",bing_api_key);
+  request.open("GET", "${bing_api_endpoint}?q=${query}", true);
+  request.setRequestHeader("Ocp-Apim-Subscription-Key",bing_api_key);
+    //   - You'll want to specify that you want json as your response type
   request.responseType = "json";
 
-  //   - You'll want to specify that you want json as your response type
+
   //   - Look for your data in event.target.response
   //   - When adding headers, also include the commented out line below. See the API docs at:
   // https://docs.microsoft.com/en-us/bing/search-apis/bing-image-search/reference/headers
@@ -31,11 +34,64 @@ function runSearch() {
   //
   // request.setRequestHeader("Ocp-Apim-Subscription-Key", bing_api_key);
 
+  
+
+  request.onload = function (){
+    if (request.status==200){ //ok
+      displayResults(request.response.value);
+    } else{
+      console.error("Error", request.statusText);
+    }
+  };
   // TODO: Send the request
+  request.send();
 
   return false;  // Keep this; it keeps the browser from sending the event
                   // further up the DOM chain. Here, we don't want to trigger
                   // the default form submission behavior.
+}
+
+function clearResultsPane() {
+  // TODO: Clear the results pane
+  const resultsImageContainer = document.querySelector("#resultsImageContainer");
+  resultsImageContainer.innerHTML = "";
+}
+
+function displayResults(results) {
+  const resultsImageContainer = document.querySelector("#resultsImageContainer");
+
+  // TODO: Loop through the results and add them to the DOM
+  results.forEach((result) => {
+    const resultImage = document.createElement("div");
+    resultImage.classList.add("resultImage");
+
+    const image = document.createElement("img");
+    image.src = result.thumbnailUrl;
+
+    resultImage.appendChild(image);
+    resultsImageContainer.appendChild(resultImage);
+
+    // TODO: Add event listeners to the images
+    image.addEventListener("click", function () {
+      addImageToBoard(result.contentUrl);
+    });
+  });
+
+  openResultsPane();
+}
+
+function addImageToBoard(imageUrl) {
+  // TODO: Implement adding the selected image to the user's mood board
+  const board = document.querySelector("#board");
+
+  const savedImage = document.createElement("div");
+  savedImage.classList.add("savedImage");
+
+  const image = document.createElement("img");
+  image.src = imageUrl;
+
+  savedImage.appendChild(image);
+  board.appendChild(savedImage);
 }
 
 function openResultsPane() {
